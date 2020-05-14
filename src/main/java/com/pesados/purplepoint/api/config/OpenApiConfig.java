beafbf859136465738a88c448619f9e9932c8b30
IsmaelDLG <ismadlg.works@gmail.com>
2020-05-14 17:06:16 +0200

    solved errors on heroku swagger

--- src/main/java/com/pesados/purplepoint/api/config/OpenApiConfig.java
+++ src/main/java/com/pesados/purplepoint/api/config/OpenApiConfig.java
@@ -22,12 +22,13 @@ import java.util.ArrayList;
 public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
-    	Server myServer = new Server();
-    	myServer.setUrl("http://10.4.41.153/");
+    	Server local = new Server();
+    	local.setUrl("http://10.4.41.153/");
+    	Server prod = new Server();
+        prod.setUrl("https://purplepoint.herokuapp.com/");
     	ArrayList<Server> serverList = new ArrayList<Server> ();
-    	serverList.add(myServer);
-        myServer.setUrl("http://localhost:5001/");
-        serverList.add(myServer);
+    	serverList.add(local);
+        serverList.add(prod);
 
         return new OpenAPI()
                 .components(new Components())
